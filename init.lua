d042dd64544e5a2e836b96219bbe7835de6ec7d3
Anup Kishore <anup.kishore@gmail.com>
2025-06-16 13:58:33 -0700

    Move autocommands to anup-the-magic.autocommands

--- init.lua
+++ init.lua
@@ -25,20 +25,6 @@ vim.g.have_nerd_font = true
 require 'anup-the-magic.options'
 require 'anup-the-magic.keymaps'
 require 'anup-the-magic.autocommands'
--- [[ Basic Autocommands ]]
---  See `:help lua-guide-autocommands`
-
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.hl.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.hl.on_yank()
-  end,
-})
-
 require 'anup-the-magic.plugins'
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
