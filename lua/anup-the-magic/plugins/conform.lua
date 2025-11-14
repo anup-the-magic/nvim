4e9a4873dffb569595995f585c36b66ba5023488
Anup Kishore <anup.kishore@gmail.com>
2025-11-14 13:29:01 -0800

    Add Conform toggle format functions

--- lua/anup-the-magic/plugins/conform.lua
+++ lua/anup-the-magic/plugins/conform.lua
@@ -6,11 +6,39 @@ return {
   keys = {
     {
       '<leader>f',
-      -- stylua: ignore start
       function() require('conform').format { async = true, lsp_format = 'fallback' } end,
-      -- stylua: ignore end
       mode = '',
-      desc = '[F]ormat buffer',
+      desc = 'Format buffer',
+    },
+    {
+      '<leader>tf',
+      function()
+        -- If autoformat is currently disabled for this buffer,
+        -- then enable it, otherwise disable it
+        if vim.b.disable_autoformat then
+          vim.cmd 'FormatEnable'
+          vim.notify 'Enabled autoformat for current buffer'
+        else
+          vim.cmd 'FormatDisable!'
+          vim.notify 'Disabled autoformat for current buffer'
+        end
+      end,
+      desc = 'Toggle autoformat for current buffer',
+    },
+    {
+      '<leader>tF',
+      function()
+        -- If autoformat is currently disabled globally,
+        -- then enable it globally, otherwise disable it globally
+        if vim.g.disable_autoformat then
+          vim.cmd 'FormatEnable'
+          vim.notify 'Enabled autoformat globally'
+        else
+          vim.cmd 'FormatDisable'
+          vim.notify 'Disabled autoformat globally'
+        end
+      end,
+      desc = 'Toggle autoformat globally',
     },
   },
   opts = {
