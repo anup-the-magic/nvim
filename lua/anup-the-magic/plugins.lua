b057f606199e1bb6c0dbbcb0f3395d8ca7f9a0b9
Anup Kishore <anup.kishore@gmail.com>
2025-12-11 14:39:43 -0800

    Add commentary, mini.statusline, remove rest of mini

--- lua/anup-the-magic/plugins.lua
+++ lua/anup-the-magic/plugins.lua
@@ -322,14 +322,39 @@ require('lazy').setup({
     end,
   },
 
-  -- Highlight todo, notes, etc in comments
+  -- Highlight todo, notes, etc in comments, like so:
+  -- TODO:
+  -- TODO(anup):
   {
     'folke/todo-comments.nvim',
     event = 'VimEnter',
     dependencies = { 'nvim-lua/plenary.nvim' },
-    opts = { signs = false },
+    opts = {
+      signs = false,
+      search = { pattern = [[\b(KEYWORDS)(\([^\)]*\))?:]] },
+      highlight = { pattern = [[.*<((KEYWORDS)%(\(.{-1,}\))?):]] },
+    },
   },
 
+  -- TODO: We're using Mini and Mini.Files
+  { -- Collection of various small independent plugins/modules
+    'echasnovski/mini.statusline',
+    lazy = false,
+    opts = {
+      use_icons = vim.g.have_nerd_font,
+    },
+    config = function(_, opts)
+      local statusline = require 'mini.statusline'
+      statusline.setup(opts)
+
+      ---@diagnostic disable-next-line: duplicate-set-field
+      statusline.section_location = function() return '%2l:%-2v' end
+    end,
+  },
+  --[[
+  -- TODO: look into this further
+  --       We're using vim-surround and vim-argumentative, which will probably be "good enough" for a while
+  --
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
     config = function()
@@ -346,25 +371,14 @@ require('lazy').setup({
       -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
-
-      -- Simple and easy statusline.
-      --  You could remove this setup call if you don't like it,
-      --  and try some other statusline plugin
-      local statusline = require 'mini.statusline'
-      -- set use_icons to true if you have a Nerd Font
-      statusline.setup { use_icons = vim.g.have_nerd_font }
-
-      -- You can configure sections in the statusline by overriding their
-      -- default behavior. For example, here we set the section for
-      -- cursor location to LINE:COLUMN
-      ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_location = function() return '%2l:%-2v' end
       -- require('mini.surround').setup()
 
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
   },
+  --]]
+
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
