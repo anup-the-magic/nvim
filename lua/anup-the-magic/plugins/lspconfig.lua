cf4291a32c70904dd8117e946d837e6f0be1e555
Anup Kishore <anup.kishore@gmail.com>
2026-06-11 09:03:56 -0700

    Remove mason

--- lua/anup-the-magic/plugins/lspconfig.lua
+++ lua/anup-the-magic/plugins/lspconfig.lua
@@ -88,10 +88,6 @@ return {
   dependencies = {
     -- TODO
     require 'anup-the-magic.plugins.telescope',
-    require 'anup-the-magic.plugins.mason',
-
-    'mason-org/mason-lspconfig.nvim',
-    'WhoIsSethDaniel/mason-tool-installer.nvim',
 
     -- Useful status updates for LSP.
     { 'j-hui/fidget.nvim', opts = {} },
@@ -192,15 +188,7 @@ return {
     --  So, we create new capabilities with blink.cmp, and then broadcast that to the servers.
     local capabilities = require('blink.cmp').get_lsp_capabilities()
 
-    -- Enable the following language servers
-    --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-    --
-    --  Add any additional override configuration in the following tables. Available keys are:
-    --  - cmd (table): Override the default command used to start the server
-    --  - filetypes (table): Override the default list of associated filetypes for the server
-    --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-    --  - settings (table): Override the default settings passed when initializing the server.
-    --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
+    -- TBD, let's figure this out
     local servers = {
       -- clangd = {},
       -- gopls = {},
@@ -233,40 +221,6 @@ return {
       pyright = {},
       -- Python formatter
       black = {},
-    }
-
-    -- Ensure the servers and tools above are installed
-    --
-    -- To check the current status of installed tools and/or manually install
-    -- other tools, you can run
-    --    :Mason
-    --
-    -- You can press `g?` for help in this menu.
-    --
-    -- `mason` had to be setup earlier: to configure its options see the
-    -- `dependencies` table for `nvim-lspconfig` above.
-    --
-    -- You can add other tools here that you want Mason to install
-    -- for you, so that they are available from within Neovim.
-    local ensure_installed = vim.tbl_keys(servers or {})
-    vim.list_extend(ensure_installed, {
-      'stylua', -- Used to format Lua code
-    })
-    require('mason-tool-installer').setup { ensure_installed = ensure_installed }
-
-    require('mason-lspconfig').setup {
-      ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
-      automatic_installation = false,
-      handlers = {
-        function(server_name)
-          local server = servers[server_name] or {}
-          -- This handles overriding only values explicitly passed
-          -- by the server configuration above. Useful when disabling
-          -- certain features of an LSP (for example, turning off formatting for ts_ls)
-          server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-          require('lspconfig')[server_name].setup(server)
-        end,
-      },
     }
 
     -- See "https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#roslyn_ls" for installation, roslyn sucks
--- lua/anup-the-magic/plugins/mason.lua
+++
@@ -1,2 +0,0 @@
--- NOTE: Mason _must_ be eagerly installed
-return { 'mason-org/mason.nvim', opts = {}, lazy = false }
